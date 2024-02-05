d270b21e8a285e926a3f47be1146d872ee134449
Boyang Zhang <bz95@cornell.edu>
2024-02-05 11:23:16 -0500

    commit

---
+++ gen_keys.py
@@ -0,0 +1,31 @@
+from web3 import Web3
+import eth_account
+import os
+
+def get_keys(challenge,keyId = 0, filename = "eth_mnemonic.txt"):
+    """
+    Generate a stable private key
+    challenge - byte string
+    keyId (integer) - which key to use
+    filename - filename to read and store mnemonics
+
+    Each mnemonic is stored on a separate line
+    If fewer than (keyId+1) mnemonics have been generated, generate a new one and return that
+    """
+
+    w3 = Web3()
+
+    msg = eth_account.messages.encode_defunct(challenge)
+
+	#YOUR CODE HERE
+
+    assert eth_account.Account.recover_message(msg,signature=sig.signature.hex()) == eth_addr, f"Failed to sign message properly"
+
+    #return sig, acct #acct contains the private key
+    return sig, eth_addr
+
+if __name__ == "__main__":
+    for i in range(4):
+        challenge = os.urandom(64)
+        sig, addr= get_keys(challenge=challenge,keyId=i)
+        print( addr )
