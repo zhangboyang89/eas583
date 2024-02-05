dbcc188a7c0b4a3081184f55e309b3f7e7eef71b
Boyang Zhang <bz95@cornell.edu>
2024-02-05 14:05:15 -0500

    commit

--- gen_keys.py
+++ gen_keys.py
@@ -1,8 +1,10 @@
 from web3 import Web3
 import eth_account
 import os
+from eth_account import Account
 
-def get_keys(challenge,keyId = 0, filename = "eth_mnemonic.txt"):
+
+def get_keys(challenge, keyId=0, filename="eth_mnemonic.txt"):
     """
     Generate a stable private key
     challenge - byte string
@@ -15,17 +17,34 @@ def get_keys(challenge,keyId = 0, filename = "eth_mnemonic.txt"):
 
     w3 = Web3()
 
+    with open(filename, 'r') as file:
+        mnemonics = file.readlines()
+
+    while len(mnemonics) <= keyId:
+        new_mnemonic = w3.eth.account.create().key.hex()
+        mnemonics.append(new_mnemonic + '\n')
+        with open(filename, 'a') as file:
+            file.write(new_mnemonic + '\n')
+
+    acct = Account.from_key(mnemonics[keyId].strip())
     msg = eth_account.messages.encode_defunct(challenge)
+    sig = acct.sign_message(msg)
 
-	#YOUR CODE HERE
+    eth_addr = acct.address
 
-    assert eth_account.Account.recover_message(msg,signature=sig.signature.hex()) == eth_addr, f"Failed to sign message properly"
+    assert eth_account.Account.recover_message(
+        msg, signature=sig.signature.hex()
+    ) == eth_addr, f"Failed to sign message properly"
 
-    #return sig, acct #acct contains the private key
+    # return sig, acct #acct contains the private key
     return sig, eth_addr
+
+
+
 
 if __name__ == "__main__":
     for i in range(4):
         challenge = os.urandom(64)
-        sig, addr= get_keys(challenge=challenge,keyId=i)
-        print( addr )
+        sig, addr = get_keys(challenge=challenge, keyId=i,
+                             filename=r'H:\My Drive\Programs\python\mcit\eas583\eth_mnemonic.txt')
+        print(addr)
