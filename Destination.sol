02dba7598ab259b73028b3ff45891a9a5f35c4fe
Boyang Zhang <bz95@cornell.edu>
2024-02-12 19:09:43 -0500

    commit

--- Destination.sol
+++ Destination.sol
@@ -34,11 +34,11 @@ contract Destination is AccessControl {
 
 	function unwrap(address _wrapped_token, address _recipient, uint256 _amount ) public {
 		require(wrapped_tokens[_wrapped_token] != address(0), "Wrapped asset not registered");
-		require(BridgeToken(_wrapped_token).balanceOf(msg.sender) >= _amount, "Insufficient balance");
 
-		BridgeToken(_wrapped_token).burn(msg.sender, _amount);
+    	// Correctly calling burnFrom with the token holder's address and the amount
+    	BridgeToken(_wrapped_token).burnFrom(msg.sender, _amount);
 
-		emit Unwrap(wrapped_tokens[_wrapped_token], _wrapped_token, msg.sender, _recipient, _amount);
+    	emit Unwrap(wrapped_tokens[_wrapped_token], _wrapped_token, msg.sender, _recipient, _amount);
 	}
 
 	function createToken(address _underlying_token, string memory name, string memory symbol ) public onlyRole(CREATOR_ROLE) returns(address) {
