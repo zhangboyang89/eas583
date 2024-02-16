d7c3b6b76eda38c7c5030aaf12278f319e66f4c8
Boyang Zhang <bz95@cornell.edu>
2024-02-16 16:08:37 -0500

    commit

--- Source.sol
+++ Source.sol
@@ -20,15 +20,21 @@ contract Source is AccessControl {
     }
 
 	function deposit(address _token, address _recipient, uint256 _amount ) public {
-		//YOUR CODE HERE
+		require(approved[_token], "Token not registered");
+		require(ERC20(_token).transferFrom(msg.sender, address(this), _amount), "Transfer failed");
+		emit Deposit(_token, _recipient, _amount);
 	}
 
 	function withdraw(address _token, address _recipient, uint256 _amount ) onlyRole(WARDEN_ROLE) public {
-		//YOUR CODE HERE
+		require(ERC20(_token).transfer(_recipient, _amount), "Transfer failed");
+    	emit Withdrawal(_token, _recipient, _amount);
 	}
 
 	function registerToken(address _token) onlyRole(ADMIN_ROLE) public {
-		//YOUR CODE HERE
+		require(!approved[_token], "Token already registered");
+		approved[_token] = true;
+		tokens.push(_token);
+		emit Registration(_token);
 	}
 
 
