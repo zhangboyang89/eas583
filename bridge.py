d123daff331fa872797fd387fe09ea10cf418f06
Boyang Zhang <bz95@cornell.edu>
2024-03-01 10:46:19 -0500

    commit

--- bridge.py
+++ bridge.py
@@ -42,36 +42,6 @@ def getContractInfo(chain):
 
     return contracts[chain]
 
-
-def getContractABI(chain):
-    """
-        Load the contract_info file into a dictinary
-        This function is used by the autograder and will likely be useful to you
-    """
-    p = Path(__file__).with_name(contract_info)
-    try:
-        with p.open('r') as f:
-            contracts = json.load(f)
-    except Exception as e:
-        print("Failed to read contract info")
-        print("Please contact your instructor")
-        print(e)
-        sys.exit(1)
-
-    if chain == 'source':
-        # look for Deposit events
-        for item in contracts[chain]['abi']:
-            if item['type'] == 'event':
-                if item['name'] == 'Deposit':
-                    return item
-    elif chain == 'destination':
-        # look for Unwrap events
-        for item in contracts[chain]['abi']:
-            if item['type'] == 'event':
-                if item['name'] == 'Unwrap':
-                    return item
-
-
 def scanBlocks(chain):
     """
         chain - (string) should be either "source" or "destination"
@@ -131,7 +101,6 @@ def scanBlocks(chain):
         return
 
     if chain == 'source':
-        event_abi = getContractABI(chain)
         contract_address = getContractInfo(chain)['address']
         contract = w3_source.eth.contract(address=contract_address, abi=source_contract_abi)
 
@@ -142,15 +111,12 @@ def scanBlocks(chain):
         event_filter = contract.events.Deposit.create_filter(fromBlock=start_block, toBlock=end_block,
                                                              argument_filters=arg_filter)
         events = event_filter.get_all_entries()
-        if len(events) > 0:
-            print('Found something!')
         for evt in events:
             call_wrap_function(evt.args['token'], evt.args['recipient'], evt.args['amount'])
 
     elif chain == 'destination':
-        event_abi = getContractABI(chain)
         contract_address = getContractInfo(chain)['address']
-        contract = w3_source.eth.contract(address=contract_address, abi=destination_contract_abi)
+        contract = w3_destination.eth.contract(address=contract_address, abi=destination_contract_abi)
 
         arg_filter = {}
         end_block = w3_destination.eth.get_block_number()
@@ -159,47 +125,45 @@ def scanBlocks(chain):
         event_filter = contract.events.Unwrap.create_filter(fromBlock=start_block, toBlock=end_block,
                                                             argument_filters=arg_filter)
         events = event_filter.get_all_entries()
-        if len(events) > 0:
-            print('Found something!')
         for evt in events:
             call_withdraw_function(evt.args['token'], evt.args['recipient'], evt.args['amount'])
 
-
-def register_token(token_address_to_register):
-    # Connect to the network
-    w3_source = connectTo('avax')
-
-    source_contract_abi = getContractInfo('source')['abi']
-    source_contract_address = getContractInfo('source')['address']
-
-    # Create contract instances
-    source_contract = w3_source.eth.contract(address=source_contract_address, abi=source_contract_abi)
-
-    # Admin account
-    private_key = '0x152d1949e8feacbf4a75acfe0679930a4c9d1bb57d88246b16aeca8627e1067b'
-    admin_account = w3_source.eth.account.from_key(private_key)
-
-    # Build transaction
-    nonce = w3_source.eth.get_transaction_count(admin_account.address)
-    transaction = source_contract.functions.registerToken(token_address_to_register).build_transaction({
-        'chainId': w3_source.eth.chain_id,
-        'gas': 200000,  # You may need to adjust this value
-        'gasPrice': w3_source.eth.gas_price,
-        'nonce': nonce,
-    })
-
-    # Sign transaction
-    signed_txn = w3_source.eth.account.sign_transaction(transaction, private_key)
-
-    # Send transaction
-    txn_hash = w3_source.eth.send_raw_transaction(signed_txn.rawTransaction)
-    print(f"Transaction hash: {txn_hash.hex()}")
-
-    # Wait for the transaction to be mined
-    txn_receipt = w3_source.eth.wait_for_transaction_receipt(txn_hash)
-    print(f"Token registered. Transaction receipt: {txn_receipt}")
-
-
-if __name__ == '__main__':
-    register_token('0xc677c31AD31F73A5290f5ef067F8CEF8d301e45c')
-    register_token('0x0773b81e0524447784CcE1F3808fed6AaA156eC8')
+#
+# def register_token(token_address_to_register):
+#     # Connect to the network
+#     w3_source = connectTo('avax')
+#
+#     source_contract_abi = getContractInfo('source')['abi']
+#     source_contract_address = getContractInfo('source')['address']
+#
+#     # Create contract instances
+#     source_contract = w3_source.eth.contract(address=source_contract_address, abi=source_contract_abi)
+#
+#     # Admin account
+#     private_key = '0x152d1949e8feacbf4a75acfe0679930a4c9d1bb57d88246b16aeca8627e1067b'
+#     admin_account = w3_source.eth.account.from_key(private_key)
+#
+#     # Build transaction
+#     nonce = w3_source.eth.get_transaction_count(admin_account.address)
+#     transaction = source_contract.functions.registerToken(token_address_to_register).build_transaction({
+#         'chainId': w3_source.eth.chain_id,
+#         'gas': 200000,  # You may need to adjust this value
+#         'gasPrice': w3_source.eth.gas_price,
+#         'nonce': nonce,
+#     })
+#
+#     # Sign transaction
+#     signed_txn = w3_source.eth.account.sign_transaction(transaction, private_key)
+#
+#     # Send transaction
+#     txn_hash = w3_source.eth.send_raw_transaction(signed_txn.rawTransaction)
+#     print(f"Transaction hash: {txn_hash.hex()}")
+#
+#     # Wait for the transaction to be mined
+#     txn_receipt = w3_source.eth.wait_for_transaction_receipt(txn_hash)
+#     print(f"Token registered. Transaction receipt: {txn_receipt}")
+#
+#
+# if __name__ == '__main__':
+#     register_token('0xc677c31AD31F73A5290f5ef067F8CEF8d301e45c')
+#     register_token('0x0773b81e0524447784CcE1F3808fed6AaA156eC8')
